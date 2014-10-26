d0bb6d89aafffa6d9136efa151a29c3aba1d2ba9
Jesse Tane <jesse.tane@gmail.com>
2014-10-26 15:35:43 -0400

    add timeout test that actually registers an event handler

--- test/timeout.js
+++ test/timeout.js
@@ -2,11 +2,50 @@ var tape = require('tape');
 var queue = require('../');
 
 tape('timeout', function(t) {
-  t.plan(3);
+  t.plan(4);
 
   var actual = [];
   var q = queue({ timeout: 100 });
   
+  q.on('timeout', function(next) {
+    t.ok(q);
+    next();
+  });
+
+  q.on('end', function() {
+    var expected = [ 'two', 'three' ];
+    t.equal(actual.length, expected.length);
+
+    for (var i in actual) {
+      var a = actual[i];
+      var e = expected[i];
+      t.equal(a, e);
+    }
+  });
+
+  q.push(function(cb) {
+    // forget to call cb
+  });
+
+  q.push(function(cb) {
+    actual.push('two');
+    cb();
+  });
+
+  q.push(function(cb) {
+    actual.push('three');
+    cb();
+  });
+
+  q.start();
+});
+
+tape('timeout auto-continue', function(t) {
+  t.plan(3);
+
+  var actual = [];
+  var q = queue({ timeout: 100 });
+
   q.on('end', function() {
     var expected = [ 'two', 'three' ];
     t.equal(actual.length, expected.length);
