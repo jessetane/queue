197e19fbda23bac199506d97efa1d69f80860a4b
kwolfy <kwolfy11@gmail.com>
2017-06-01 11:56:03 +0300

    promises support

--- index.js
+++ index.js
@@ -127,7 +127,14 @@ Queue.prototype.start = function (cb) {
   }
 
   this.pending++
-  job(next)
+  var promise = job(next)
+  if (promise && promise.then && promise.catch) {
+    promise.then(function (result) {
+      next(null, result)
+    }).catch(function (err) {
+      next(err || true)
+    })
+  }
 
   if (this.jobs.length > 0) {
     this.start()
