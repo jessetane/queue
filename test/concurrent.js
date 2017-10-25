c47bec6a718654a10ba19913504c92f4e3b44ffc
Steven de Salas <sdesalas@users.noreply.github.com>
2017-10-25 17:13:20 +1100

    Update concurrent.js

--- test/concurrent.js
+++ test/concurrent.js
@@ -2,18 +2,10 @@ var tape = require('tape')
 var queue = require('../')
 
 tape('concurrent', function (t) {
-  t.plan(7)
+  t.plan(4)
 
   var actual = []
   var q = queue()
-  q.concurrency = 2
-
-  q.push(function (cb) {
-    setTimeout(function () {
-      actual.push('four')
-      cb()
-    }, 20)
-  })
 
   q.push(function (cb) {
     setTimeout(function () {
@@ -24,35 +16,96 @@ tape('concurrent', function (t) {
 
   q.push(function (cb) {
     setTimeout(function () {
+      actual.push('three')
+      cb()
+    }, 20)
+  })
+
+  q.push(function (cb) {
+    setTimeout(function () {
       actual.push('two')
+      cb()
+    }, 10)
+  })
+
+  q.start(function () {
+    var expected = [ 'one', 'two', 'three' ]
+    t.equal(actual.length, expected.length)
+
+    for (var i in actual) {
+      var a = actual[i]
+      var e = expected[i]
+      t.equal(a, e)
+    }
+  })
+})
+
+tape('concurrent with changes', function (t) {
+  t.plan(13)
+
+  var actual = []
+  var q = queue()
+  q.concurrency = 2
+
+  q.push(function (cb) {
+    actual.push('one.start')
+    setTimeout(function () {
+      actual.push('one.end')
+      cb()
+    }, 20)
+  })
+
+  q.push(function (cb) {
+    actual.push('two.start')
+    setTimeout(function () {
+      actual.push('two.end')
       cb()
     }, 0)
   })
 
   q.push(function (cb) {
+    actual.push('three.start')
+    setTimeout(function () {
+      actual.push('three.end')
+      cb()
+    }, 0)
+  })
+
+  q.push(function (cb) {
+    actual.push('four.start')
     q.concurrency = 1
     setTimeout(function () {
-      actual.push('three')
+      actual.push('four.end')
       cb()
     }, 10)
   })
 
   q.push(function (cb) {
+    actual.push('five.start')
     setTimeout(function () {
-      actual.push('five')
+      actual.push('five.end')
       cb()
     }, 30)
   })
 
   q.push(function (cb) {
+    actual.push('six.start')
     setTimeout(function () {
-      actual.push('six')
+      actual.push('six.end')
       cb()
     }, 0)
   })
 
   q.start(function () {
-    var expected = [ 'one', 'two', 'three', 'four', 'five', 'six' ]
+    var expected = [
+      'one.start',
+      'two.start', 'two.end',
+      'three.start', 'three.end',
+      'four.start', 'four.end', // <-- concurrency dropped
+      'one.end',
+      'five.start', 'five.end',
+      'six.start', 'six.end'
+    ]
     t.equal(actual.length, expected.length)
 
     for (var i in actual) {
